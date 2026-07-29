bf5e519b422557fd608bfa5c6c76fa088c3406c9
Emile Nols <emilenols@gmail.com>
2026-07-29 17:43:32 +0200

    chore: Clean up temp scripts

--- add_html_gate_class.py
+++
@@ -1,7 +0,0 @@
-for fname in ['index.html', 'members.html']:
-    with open(fname, 'r', encoding='utf-8') as f:
-        content = f.read()
-    content = content.replace('<html lang="en">', '<html lang="en" class="locked-gate">')
-    with open(fname, 'w', encoding='utf-8') as f:
-        f.write(content)
-    print(f"Updated {fname} with class locked-gate on <html> tag")
